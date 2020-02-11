31df3fe047d437fb590cb6dc1e568414e9f0e3eb
Ahmet Ozcelik <ahmetozcelik07@gmail.com>
2020-02-11 09:25:11 +0300

    webpack eklendi. configler duzenlendi

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: "inline-source-map",
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    })],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
+        ],
+    },
+    watch: true,
+    devServer: {
+        port:1453,
+        index: "index.html",
+        open: true
+    }
+}
+
