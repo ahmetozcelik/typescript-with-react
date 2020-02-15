943d83b8758ddda6900077cf847a362d33d3d16c
Ahmet Ozcelik <ahmetozcelik07@gmail.com>
2020-02-15 21:41:30 +0300

    bootstrap eklendi. search bar eklendi

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,59 @@
-import React from "react";
-import { Component } from "react";
+import React, { Component, BlockquoteHTMLAttributes } from "react";
+
+class Search extends Component<{ books }, {}> {
+  state = {
+    search: ""
+  };
+  handleChange = e => {
+    this.setState({ search: e.target.value });
+  };
+  render() {
+    const { search } = this.state;
+    const filteredBooks = this.props.books.filter(book => {
+      return (
+        book.title.toLowerCase().indexOf(search.toLowerCase()) !== -1 ||
+        book.author.toLowerCase().indexOf(search.toLowerCase()) !== -1 ||
+        book.publisher.toLowerCase().indexOf(search.toLowerCase()) !== -1
+        )
+    });
+    return (
+      <div>
+        <div className="md-form mt-0">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Ara"
+            onChange={this.handleChange}
+          />
+        </div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Kitap Adı</th>
+              <th scope="col">Yazar</th>
+              <th scope="col">Yayınevi</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredBooks.map(book => (
+              <tr key={book.id}>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>{book.publisher}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 const Main = ({ books }) => {
   return (
-    <div>
-      <h1>Books List</h1>
-      {books.map(book => (
-        <div>{book.title}</div>
-      ))}
+    <div className="w-50 mx-auto">
+      <h1>Kitap Listesi</h1>
+      <Search books={books} />
     </div>
   );
 };
